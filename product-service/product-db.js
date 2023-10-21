dc9f8ea786ec2636860a504d3a14ea8256bfa1d0
Parth Sanaja <parth.sanaja@turtlemint.com>
2023-10-21 09:33:26 +0530

    product service db file

---
+++ product-service/product-db.js
@@ -0,0 +1,351 @@
+use product-service;
+db.product.insert({
+    "_id" : ObjectId("649e5260050438228ef6f08a"),
+    "name" : "Apple macbook Pro M1",
+    "description" : "2021 launch from Apple",
+    "price" : 12000.0,
+    "_class" : "com.example.productservice.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("649fe4100d5a2d759a9361ab"),
+    "name" : "Apple macbook Pro M2",
+    "description" : "2023 launch from Apple",
+    "price" : 15000.0,
+    "_class" : "com.example.productservice.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("649fe43e0d5a2d759a9361ac"),
+    "name" : "Apple Iphone 14",
+    "description" : "iPhone 14",
+    "price" : 8000.0,
+    "_class" : "com.example.productservice.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("649fe44d0d5a2d759a9361ad"),
+    "name" : "Apple Iphone 13",
+    "description" : "iPhone 13",
+    "price" : 50000.0,
+    "_class" : "com.example.productservice.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("649ff930838f01086b895604"),
+    "name" : "Apple",
+    "description" : "Apple Gala",
+    "price" : 140.0,
+    "_class" : "com.example.productservice.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("649ff930838f01086b895605"),
+    "name" : "Mango",
+    "description" : "Mango Devgadh",
+    "price" : 600.0,
+    "_class" : "com.example.productservice.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64b6079ed0f9d76f51f59001"),
+    "name" : "Mango",
+    "description" : "Mango Devgadh",
+    "price" : 600.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64b6079ed0f9d76f51f59002"),
+    "name" : "Grapes",
+    "description" : "Grapes from Masic",
+    "price" : 300.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64b607a9d0f9d76f51f59003"),
+    "name" : "Apple Iphone 13",
+    "description" : "iPhone 13",
+    "price" : 50000.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64b757ba13688835faa05f19"),
+    "name" : "Grapes",
+    "description" : "Grapes from Masic",
+    "price" : 300.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64b757ba13688835faa05f18"),
+    "name" : "Mango",
+    "description" : "Mango Devgadh",
+    "price" : 600.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64b757ba13688835faa05f17"),
+    "name" : "Apple",
+    "description" : "Apple Gala",
+    "price" : 140.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64b7e745305a5264c27ea454"),
+    "name" : "Grapes",
+    "description" : "Grapes from Masic",
+    "price" : 300.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64b7e745305a5264c27ea452"),
+    "name" : "Apple",
+    "description" : "Apple Gala",
+    "price" : 140.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64b7e745305a5264c27ea453"),
+    "name" : "Mango",
+    "description" : "Mango Devgadh",
+    "price" : 600.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64ca5ce0e7346b554e242b93"),
+    "name" : "Apple",
+    "description" : "Apple Gala",
+    "price" : 140.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64ca5ce0e7346b554e242b94"),
+    "name" : "Mango",
+    "description" : "Mango Devgadh",
+    "price" : 600.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64ca5ce0e7346b554e242b95"),
+    "name" : "Grapes",
+    "description" : "Grapes from Masic",
+    "price" : 300.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64ca61d51403cf67c201ed1e"),
+    "name" : "Grapes",
+    "description" : "Grapes from Masic",
+    "price" : 300.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64ca61d51403cf67c201ed1d"),
+    "name" : "Mango",
+    "description" : "Mango Devgadh",
+    "price" : 600.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64ca61d51403cf67c201ed1c"),
+    "name" : "Apple",
+    "description" : "Apple Gala",
+    "price" : 140.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64ca62bc4a2a5f5ac28d831d"),
+    "name" : "Grapes",
+    "description" : "Grapes from Masic",
+    "price" : 300.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64ca62bc4a2a5f5ac28d831c"),
+    "name" : "Mango",
+    "description" : "Mango Devgadh",
+    "price" : 600.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64ca62bc4a2a5f5ac28d831b"),
+    "name" : "Apple",
+    "description" : "Apple Gala",
+    "price" : 140.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64ca62cc1268284d581bb68e"),
+    "name" : "Mango",
+    "description" : "Mango Devgadh",
+    "price" : 600.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64ca62cc1268284d581bb68d"),
+    "name" : "Apple",
+    "description" : "Apple Gala",
+    "price" : 140.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64ca62cc1268284d581bb68f"),
+    "name" : "Grapes",
+    "description" : "Grapes from Masic",
+    "price" : 300.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64cc7045f9c2f732444bd81b"),
+    "name" : "Grapes",
+    "description" : "Grapes from Masic",
+    "price" : 300.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64cc7045f9c2f732444bd81a"),
+    "name" : "Mango",
+    "description" : "Mango Devgadh",
+    "price" : 600.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("64cc7045f9c2f732444bd819"),
+    "name" : "Apple",
+    "description" : "Apple Gala",
+    "price" : 140.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("65098a9afe122860f7cba5b4"),
+    "name" : "Grapes",
+    "description" : "Grapes from Masic",
+    "price" : 300.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("65098a9afe122860f7cba5b3"),
+    "name" : "Mango",
+    "description" : "Mango Devgadh",
+    "price" : 600.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("65098a9afe122860f7cba5b2"),
+    "name" : "Apple",
+    "description" : "Apple Gala",
+    "price" : 140.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("6509a218ba29a664ceb80d61"),
+    "name" : "Apple",
+    "description" : "Apple Gala",
+    "price" : 140.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("6509a218ba29a664ceb80d63"),
+    "name" : "Grapes",
+    "description" : "Grapes from Masic",
+    "price" : 300.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("6509a218ba29a664ceb80d62"),
+    "name" : "Mango",
+    "description" : "Mango Devgadh",
+    "price" : 600.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("6513a7cc34155f0192a72cd3"),
+    "name" : "Apple",
+    "description" : "Apple Gala",
+    "price" : 140.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("6513a7cc34155f0192a72cd5"),
+    "name" : "Grapes",
+    "description" : "Grapes from Masic",
+    "price" : 300.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("6513a7cc34155f0192a72cd4"),
+    "name" : "Mango",
+    "description" : "Mango Devgadh",
+    "price" : 600.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("651918eed41ac827869f7b77"),
+    "name" : "Apple",
+    "description" : "Apple Gala",
+    "price" : 140.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("651918eed41ac827869f7b78"),
+    "name" : "Mango",
+    "description" : "Mango Devgadh",
+    "price" : 600.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("651918eed41ac827869f7b79"),
+    "name" : "Grapes",
+    "description" : "Grapes from Masic",
+    "price" : 300.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("65191a716a306463dd040920"),
+    "name" : "Grapes",
+    "description" : "Grapes from Masic",
+    "price" : 300.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("65191a716a306463dd04091f"),
+    "name" : "Mango",
+    "description" : "Mango Devgadh",
+    "price" : 600.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("65191a716a306463dd04091e"),
+    "name" : "Apple",
+    "description" : "Apple Gala",
+    "price" : 140.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("65191b70caf21c432f37c4c9"),
+    "name" : "Mango",
+    "description" : "Mango Devgadh",
+    "price" : 600.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("65191b70caf21c432f37c4ca"),
+    "name" : "Grapes",
+    "description" : "Grapes from Masic",
+    "price" : 300.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("65191b70caf21c432f37c4c8"),
+    "name" : "Apple",
+    "description" : "Apple Gala",
+    "price" : 140.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("651965ffac28926563bd4752"),
+    "name" : "Apple",
+    "description" : "Apple Gala",
+    "price" : 140.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
+db.product.insert({
+    "_id" : ObjectId("651965ffac28926563bd4754"),
+    "name" : "Grapes",
+    "description" : "Grapes from Masic",
+    "price" : 300.0,
+    "_class" : "org.parths.productservicev2.entity.Product"
+});
